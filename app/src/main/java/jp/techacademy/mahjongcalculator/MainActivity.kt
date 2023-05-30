9494fef9a94e15cdfe3edd2473f01a56c9f63d70
t-tomizawa <t-tomizawa@i-enter.co.jp>
2023-05-30 09:37:47 +0900

    種類ごとにRecyclerViewを作成して、画像を表示させる

--- app/src/main/java/jp/techacademy/mahjongcalculator/MainActivity.kt
+++ app/src/main/java/jp/techacademy/mahjongcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.techacademy.mahjongcalculator
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,34 +11,69 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.RecyclerListener
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import jp.techacademy.mahjongcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var tileAdapter: TileAdapter
+    private lateinit var recyclerViewManzu: RecyclerView
+    private lateinit var recyclerViewPinzu: RecyclerView
+    private lateinit var recyclerViewSouzu: RecyclerView
+
+    private lateinit var manzuAdapter: TileAdapter
+    private lateinit var pinzuAdapter: TileAdapter
+    private lateinit var souzuAdapter: TileAdapter
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        recyclerView = binding.recyclerViewTiles
-        recyclerView.layoutManager = GridLayoutManager(this, 7)
-        tileAdapter = TileAdapter(getTileList())
-        recyclerView.adapter = tileAdapter
+        recyclerViewManzu = binding.recyclerViewManzu
+        recyclerViewPinzu = binding.recyclerViewPinzu
+        recyclerViewSouzu = binding.recyclerViewSouzu
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerViewManzu.layoutManager = GridLayoutManager(this, 9)
+        recyclerViewPinzu.layoutManager = GridLayoutManager(this, 9)
+        recyclerViewSouzu.layoutManager = GridLayoutManager(this, 9)
+
+        manzuAdapter = TileAdapter(getTiles("manzu"))
+        pinzuAdapter = TileAdapter(getTiles("pinzu"))
+        souzuAdapter = TileAdapter(getTiles("souzu"))
+
+        recyclerViewManzu.adapter = manzuAdapter
+        recyclerViewPinzu.adapter = pinzuAdapter
+        recyclerViewSouzu.adapter = souzuAdapter
     }
 
-    private fun getTileList(): List<Int> {
-        return listOf(
-            R.drawable.aka1_66_90_l_emb,
-            R.drawable.ji4_66_90_l_emb
-        )
+    private fun getTiles(tileType: String): List<Int> {
+        val tileList = mutableListOf<Int>()
+        val resourcePrefix = "tiles_"
+
+        // 1-9までの牌を取得する
+        for (i in 1..9) {
+            val resourceName = resourcePrefix + tileType + "_" + i.toString()
+            val resourceId = resources.getIdentifier(resourceName, "drawable", packageName)
+            tileList.add(resourceId)
+        }
+
+        /*// 筒子の牌を9筒まで取得する
+        for (i in 1..9) {
+            val resourceName = resourcePrefix + "pinzu_" + i.toString()
+            val resourceId = resources.getIdentifier(resourceName, "drawable", packageName)
+            tileList.add(resourceId)
+        }
+
+        // 索子の牌を9索まで取得する
+        for (i in 1..9) {
+            val resourceName = resourcePrefix + "souzu_" + i.toString()
+            val resourceId = resources.getIdentifier(resourceName, "drawable", packageName)
+            tileList.add(resourceId)
+        }*/
+
+        return tileList
     }
 
     private inner class TileAdapter(private val tileList: List<Int>) :
@@ -51,20 +87,11 @@ class MainActivity : AppCompatActivity() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TileViewHolder {
             val itemView =
                 LayoutInflater.from(parent.context).inflate(R.layout.item_tile, parent, false)
-            val layoutParams = itemView.layoutParams as ViewGroup.MarginLayoutParams
-            layoutParams.setMargins(0, 0, 0, 50.dpToPx())
-            itemView.layoutParams = layoutParams
             return TileViewHolder(itemView)
         }
 
         override fun getItemCount() = tileList.size
 
-
         inner class TileViewHolder(itemView: View) : ViewHolder(itemView)
     }
-}
-
-fun Int.dpToPx(): Int {
-    val density = Resources.getSystem().displayMetrics.density
-    return (this * density).toInt()
 }
