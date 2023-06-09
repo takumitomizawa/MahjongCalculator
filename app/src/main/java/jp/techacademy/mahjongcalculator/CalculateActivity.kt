db3a5c6df7d0f24b4720f23635a75a77aa1d614c
t-tomizawa <t-tomizawa@i-enter.co.jp>
2023-06-09 16:46:13 +0900

    飜数と符数を切り替えるSwitchを実装

--- app/src/main/java/jp/techacademy/mahjongcalculator/CalculateActivity.kt
+++ app/src/main/java/jp/techacademy/mahjongcalculator/CalculateActivity.kt
@@ -3,6 +3,7 @@ package jp.techacademy.mahjongcalculator
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,6 +16,7 @@ class CalculateActivity : AppCompatActivity() {
     private lateinit var recyclerViewResultHand: RecyclerView
     private lateinit var resultAdapter: TileAdapter
     private lateinit var binding: ActivityCalculateBinding
+    private var isShowingYaku = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +35,19 @@ class CalculateActivity : AppCompatActivity() {
 
         val linearLayoutColumn1 = binding.yakuColumn1
         val linearLayoutColumn2 = binding.yakuColumn2
+        val switch = binding.yakuSwitch
 
         val textSizeInDp = 25
+
+        switch.setOnCheckedChangeListener{ _, isChecked ->
+            isShowingYaku = isChecked
+
+            linearLayoutColumn1.visibility = if (isShowingYaku) View.VISIBLE else View.GONE
+            linearLayoutColumn2.visibility = if (isShowingYaku) View.VISIBLE else View.GONE
+        }
+
+        linearLayoutColumn1.visibility = View.GONE
+        linearLayoutColumn2.visibility = View.GONE
 
         for ((index, yaku) in testYakuList.withIndex()) {
             val textView = TextView(this)
@@ -44,8 +57,18 @@ class CalculateActivity : AppCompatActivity() {
 
             if (index < 5) {
                 linearLayoutColumn1.addView(textView)
+                if (isShowingYaku){
+                    textView.visibility = View.VISIBLE
+                } else {
+                    textView.visibility = View.GONE
+                }
             } else {
                 linearLayoutColumn2.addView(textView)
+                if (isShowingYaku){
+                    textView.visibility = View.VISIBLE
+                } else {
+                    textView.visibility = View.GONE
+                }
             }
         }
     }
